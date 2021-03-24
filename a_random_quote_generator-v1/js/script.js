bcd202861bbbe520418f0fcff358ddc38924cf2e
Joe Murray <murrayj42@gmail.com>
2021-03-24 09:18:02 -0400

    final touches

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -3,27 +3,7 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-  
-  
-  
-  
-  
-  
-  
-  function random_bg_color() { // this function creates a random background color and will be used in the print quote function 
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    document.body.style.background = bgColor;
-    }
-/*** 
- * `quotes` array 
-***/
-let quotes = // an array of quotes and tags about the quotes
+let quotes = // an array of quotes + info about them. 
 [ 
   { quote: ' "No one is going to value you more than you value yourself." ',
     source: 'Naval Ravikant',
@@ -57,36 +37,30 @@ let quotes = // an array of quotes and tags about the quotes
   } 
 ];
 
-/***
- * `getRandomQuote` function
-***/
+function getRandomQuote() {return quotes[Math.floor(Math.random() * quotes.length )]} // returns random quote in array. Will use in printQuote function
 
-function getRandomQuote() {return quotes[Math.floor(Math.random() * quotes.length )]} // returns random quote in array
-/***
- * `printQuote` function
-***/
+function random_bg_color() { // this function creates a random background color and will be used in the print quote function 
+  var r = Math.floor(Math.random() * 256); 
+  var g = Math.floor(Math.random() * 256); 
+  var b = Math.floor(Math.random() * 256); 
+  var bgColor = `rgb(${r},${g},${b})`; // random rgb value 
+document.body.style.background = bgColor; // changes 
+  } // here's more info about it: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
 
 function printQuote() {
-  var randomQuote = getRandomQuote(); // returns randomQuote function
-  var html = // concatonating data into html template literals
-  ` <p class = "quote"> ${randomQuote.quote} </p>
-    <p class = "source"> ${randomQuote.source} 
-        <span>${randomQuote.year}</span>
-        <span> ${randomQuote.citation}</span>
-    </p>
-    <p class = "tags"> Tags: ${randomQuote.tag} </p> 
-  `; // end of template literals html
-  
+  var randomQuote = getRandomQuote(); // returns randomQuote 
+  var html = // concatenates data into html template literals
+    ` <p class = "quote"> ${randomQuote.quote} </p>
+      <p class = "source"> ${randomQuote.source} 
+          <span>${randomQuote.year}</span>
+          <span> ${randomQuote.citation}</span>
+      </p>
+      <p class = "tags"> Tags: ${randomQuote.tag} </p> 
+    `; // end of template literals html
   document.getElementById('quote-box').innerHTML = html; // inserts html code into the html file 
+  random_bg_color(); // random background color funtion at top of js file
+  };
 
-  random_bg_color();
-}; // time in miliseconds 
+setInterval(printQuote, 10000); // calls printQuote function every 10,000ms (10secs)
 
-setInterval(printQuote, 10000); // executes printQuote function at this interval in milliseconds 
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false); // click event listener for the print quote button 
